e14868d20b598b5850434bcc0b5b0576ccaa2b90
Benjamin Gross <ben@benbox.org>
2025-01-17 20:44:40 +0100

    added week 2 Tutorial

--- Week 2.R
+++ Week 2.R
@@ -4,3 +4,16 @@ here::here('figures', 'image-W2-Topic3.png')
 
 # Reprex
 library(reprex)
+library(tidyverse)
+
+# Explicit for loop
+for (idx in 1:5) {
+  cat(sum(iris[idx]), '\n')
+}
+
+count_rows <- function(x) cat(sum(iris[x]), '\n')
+# Sort of "hidden" loop
+walk(1:4, ~count_rows(.x) )
+
+# Actually ectoized function
+colSums(iris[-5])
