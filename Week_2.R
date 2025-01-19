687d9ff75bb830eb7ad46b2a76140c85d08302ae
Benjamin Gross <ben@benbox.org>
2025-01-19 12:32:46 +0100

    added underscores to file names

---
+++ Week_2.R
@@ -0,0 +1,68 @@
+# Intermediate R Programming for Data Science 
+# Week 2
+here::here('figures', 'image-W2-Topic3.png')
+
+
+## Reprex
+library(reprex)
+library(tidyverse)
+library(here)
+
+# Explicit for loop
+for (idx in 1:5) {
+  cat(sum(iris[idx]), '\n')
+}
+
+count_rows <- function(x) cat(sum(iris[x]), '\n')
+# Sort of "hidden" loop
+walk(1:4, ~count_rows(.x) )
+
+# Actually ectoized function
+colSums(iris[-5])
+
+# Use Add-ins in Rstudio
+# to use reprex
+
+
+## Sourcing code
+source(here("Week_2_sourced_file.R"))
+sourced_func(4)
+
+
+## Documentation
+?if_else # vectorised if-else from dplyr
+?ifelse # base package
+
+# Help
+help("ifelse") # help
+?ifelse # help
+
+# Under the hood
+View(if_else)
+if_else # run or press F2 with the mouse cursor over it 
+
+# Vignettes
+browseVignettes()
+vignette("rectangle", package = "tidyr")
+
+
+## Debugging
+# Line by line debugging
+# Highlight section
+
+# Usage of `print()`
+
+# Sprinkle `str()`
+mtcars %>%
+  str()
+
+# Usage of `traceback()`
+f <- function(x) {
+  x + 1
+}
+
+g <- function(x) f(x)
+
+g(1)
+g("a")
+traceback()
---
+++ Week_2_sourced_file.R
@@ -0,0 +1,4 @@
+# File for sourcing in
+sourced_func <- function(x) {
+  x^2
+}
