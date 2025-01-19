1e4cc2fd92be6f3bb99c9707d8e2a3349ee4fbc4
Benjamin Gross <ben@benbox.org>
2025-01-19 18:48:21 +0100

    added more content

--- Week_2.R
+++ Week_2.R
@@ -48,8 +48,14 @@ vignette("rectangle", package = "tidyr")
 
 ## Debugging
 # Line by line debugging
-# Highlight section
+# just highlight section
 
+# Interactive debugger
+# add `browser()` to a function
+# or set a red dot next to the line number in the editor window
+# commands:
+ls() # showing all variables / objects 
+ls.str() # exploring all variables / objects
 # Usage of `print()`
 
 # Sprinkle `str()`
@@ -65,4 +71,8 @@ g <- function(x) f(x)
 
 g(1)
 g("a")
+traceback()
+
+sourced_func(2)
+sourced_func("a")
 traceback()
--- Week_2_sourced_file.R
+++ Week_2_sourced_file.R
@@ -1,4 +1,6 @@
 # File for sourcing in
 sourced_func <- function(x) {
+  x <- x^2
+  print("Hi")
   x^2
 }
